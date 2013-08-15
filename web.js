63fb9139e6b7c2b95fbd522f705032d24a9ca32f
Damien E <damien.eggenspieler@gmail.com>
2013-08-15 01:33:26 +0000

    new web.js

--- web.js
+++ web.js
@@ -1,9 +1,14 @@
 var express = require('express');
+var fs = require('fs');
+
+var data = fs.readFileSync("index.html",'"utf-8");
+
+
 var app = express();
 app.use(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(data);
 });
 
 var port = process.env.PORT || 5000;
