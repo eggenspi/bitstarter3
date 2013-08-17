d2a501f0a90958f046baa8bbd878406672cffcde
Ubuntu <ubuntu@ip-172-31-39-37.us-west-2.compute.internal>
2013-08-17 17:36:42 +0000

    new web without CSS in html

--- web.js
+++ web.js
@@ -1,10 +1,9 @@
 var express = require('express');
+var app = express();
+app.use(express.logger());
+
 var fs = require('fs');
-
-var data = fs.readFileSync("index.html","utf-8");
-
-
-var app = express.createServer(express.logger());
+var data = fs.readFileSync("index2.html","utf-8");
 
 app.get('/', function(request, response) {
   response.send(data);
