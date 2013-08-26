470c409c56e4c6a132096d03f27d11bffc9b6e82
Ubuntu <ubuntu@ip-172-31-39-37.us-west-2.compute.internal>
2013-08-26 15:45:58 +0000

    new web manu

--- web.js
+++ web.js
@@ -5,13 +5,18 @@ app.use(express.logger());
 var fs = require('fs');
 var data = fs.readFileSync("manufacturing.html","utf-8");
 var data2 = fs.readFileSync("technology.html","utf-8");
+var data3 = fs.readFileSync("contact.html","utf-8");
 
 app.get('/', function(request, response) {
   response.send(data);
 });
 
 app.get('/technology.html', function(request, response) {
-  response.send(data2);
+  response.send(data);
+});
+
+app.get('/contact.html', function(request, response) {
+  response.send(data);
 });
 
 var port = process.env.PORT || 8080;
